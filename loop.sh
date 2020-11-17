7e92339a8247bf4e338f29671fc05dbbdbba0a5b
markfirmware <markfirmware@users.noreply.github.com>
2020-11-16 22:42:25 -0500

    update

--- loop.sh
+++ loop.sh
@@ -1,61 +1,12 @@
 #!/bin/bash
 set -e
 
-function run {
-    rm -rf app.git
-    npx create-snowpack-app --template @snowpack/app-template-blank app.git
-    pushd app.git
-        npm run build
-    popd
-}
-
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
-run
+for i in $(seq 1 20)
+do
+  echo $i
+  rm -rf app.git
+  npx create-snowpack-app --template @snowpack/app-template-blank app.git
+  pushd app.git
+      npm run build
+  popd
+done
