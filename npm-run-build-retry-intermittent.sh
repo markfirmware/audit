c0e1b6634a9272e7a0f2670da01a8db4ea050237
markfirmware <mark.kendrat@gmail.com>
2020-11-20 05:03:04 +0000

    update

--- npm-run-build-retry-intermittent.sh
+++ npm-run-build-retry-intermittent.sh
@@ -11,7 +11,8 @@
         cat npm.run.build.log
         echo npm.run.build.log ...........................................
 #       cat npm.run.build.log | sed 1,/npx:.installed/d | sed s/.*Z// | sed /added/d > error.log
-        cat npm.run.build.log | sed 1,/TypeError/d | sed /npm.ERR!.code.ELIFECYCLE/,$d > error.log
+#       cat npm.run.build.log | sed 1,/TypeError:/d | sed /npm.ERR!.code.ELIFECYCLE/,$d > error.log
+        cat npm.run.build.log | sed 1,/TypeError:/d > error.log
         echo error.log ...................................................
         cat error.log
         echo error.log ...................................................
