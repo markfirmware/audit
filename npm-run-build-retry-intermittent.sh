6ed0f9bcc3add5f266655ad7f31f388452865c01
markfirmware <mark.kendrat@gmail.com>
2020-11-20 05:48:29 +0000

    update

--- npm-run-build-retry-intermittent.sh
+++ npm-run-build-retry-intermittent.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+cat << __EOF__ > intermittent-error.log
+TypeError: undefined is not a function
+    at parseJsForInstallTargets (/home/runner/work/audit/audit/app/node_modules/snowpack/lib/scan-imports.js:114:19)
+    at parseFileForInstallTargets (/home/runner/work/audit/audit/app/node_modules/snowpack/lib/scan-imports.js:150:24)
+    at /home/runner/work/audit/audit/app/node_modules/snowpack/lib/scan-imports.js:237:30
+    at Array.map (<anonymous>)
+    at Object.scanImportsFromFiles (/home/runner/work/audit/audit/app/node_modules/snowpack/lib/scan-imports.js:237:10)
+    at Object.getInstallTargets (/home/runner/work/audit/audit/app/node_modules/snowpack/lib/commands/install.js:47:57)
+    at installOptimizedDependencies (/home/runner/work/audit/audit/app/node_modules/snowpack/lib/commands/build.js:69:44)
+    at installDependencies (/home/runner/work/audit/audit/app/node_modules/snowpack/lib/commands/build.js:327:37)
+    at Object.command (/home/runner/work/audit/audit/app/node_modules/snowpack/lib/commands/build.js:381:31)
+    at async cli (/home/runner/work/audit/audit/app/node_modules/snowpack/lib/index.js:157:9)
+__EOF__
+
 #   ok=1
 #   while [[ $ok != 0 ]]
 #   do
@@ -8,9 +22,12 @@
     if [[ $ok != 0 ]]
     then
         cat npm.run.build.log | sed 1,/index.html/d | sed /npm.ERR!.code.ELIFECYCLE/,\$d > error.log
-        echo error.log ...................................................
-        cat error.log
-        echo error.log ...................................................
+        diff error.log intermittent-error.log
+        if [[ $ok == 0 ]]
+        then
+	    echo retry intermittent error ...
+	fi
+        cat npm-run-build.log
         exit 1
     fi
 #   done
