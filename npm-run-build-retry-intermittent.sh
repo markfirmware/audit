5c057b2a7c9d04ce38874f4c1460dfac0689fa69
markfirmware <mark.kendrat@gmail.com>
2020-11-20 04:53:18 +0000

    update

--- npm-run-build-retry-intermittent.sh
+++ npm-run-build-retry-intermittent.sh
@@ -7,11 +7,14 @@
     ok=$?
     if [[ $ok != 0 ]]
     then
-        echo npm.run.build.log #########
+        echo npm.run.build.log ...........................................
         cat npm.run.build.log
-        cat npm.run.build.log | sed 1,/npx:.installed/d | sed s/.*Z// | sed /added/d > error.log
-        echo error.log #################
+        echo npm.run.build.log ...........................................
+#       cat npm.run.build.log | sed 1,/npx:.installed/d | sed s/.*Z// | sed /added/d > error.log
+        cat npm.run.build.log | sed 1,/npx:.installed/d | sed /added/d > error.log
+        echo error.log ...................................................
         cat error.log
+        echo error.log ...................................................
         exit 1
     fi
 #   done
