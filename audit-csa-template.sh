c483c3dc4cc874836fe3d4582148f5d524bd001f
markfirmware <mark.kendrat@gmail.com>
2020-12-14 16:39:25 +0000

    update

--- audit-csa-template.sh
+++ audit-csa-template.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-set -e
 
 templates=$(npm search --json --searchlimit 100 keywords:csa-template | jq '.[] | .name' | sort)
 for template in $templates
 do
     rm -rf app
-    clear
     echo $template
     npx create-snowpack-app --template $template app
     pushd app
