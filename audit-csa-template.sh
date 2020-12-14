9d65623e4c16752a2980876a23f6cc57aa690697
markfirmware <markfirmware@users.noreply.github.com>
2020-12-14 07:21:48 -0500

    audit-csa-template.sh

---
+++ audit-csa-template.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+templates=$(npm search --json keywords:csa-template | jq '.[] | .name' | sort)
+for template in $templates
+do
+    rm -rf app
+    clear
+    echo $template
+    npx create-snowpack-app --template $template app
+    pushd app
+        npm run build
+    popd
+done
